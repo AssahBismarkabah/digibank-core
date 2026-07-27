0373080f1f5fece446e269a35c57da937bdad9c0
Assah Bismark <assah9799@gmail.com>
2026-07-27 16:32:06 +0100

    Fix duplicate JPA repository scanning

--- digibank-parent/digibank-transaction/src/main/java/com/digibank/transaction/TransactionApplication.java
+++ digibank-parent/digibank-transaction/src/main/java/com/digibank/transaction/TransactionApplication.java
@@ -5,9 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.persistence.autoconfigure.EntityScan;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(scanBasePackages = "com.digibank")
+@SpringBootApplication
+@ComponentScan(basePackages = "com.digibank",
+        excludeFilters = @ComponentScan.Filter(type = FilterType.REGEX, pattern = "com\\.digibank\\..*Application"))
 @EnableJpaRepositories(basePackages = "com.digibank")
 @EntityScan(basePackages = "com.digibank")
 public class TransactionApplication extends SpringBootServletInitializer {
