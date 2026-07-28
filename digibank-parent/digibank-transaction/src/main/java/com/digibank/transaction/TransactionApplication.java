3c3e04b6dfc0446849cef125e9b7225b42277cc9
tarthur <tcheutchouaarthur38@gmail.com>
2026-07-28 11:52:01 +0100

    fixing the smoke test

--- digibank-parent/digibank-transaction/src/main/java/com/digibank/transaction/TransactionApplication.java
+++ digibank-parent/digibank-transaction/src/main/java/com/digibank/transaction/TransactionApplication.java
@@ -2,9 +2,7 @@ package com.digibank.transaction;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.persistence.autoconfigure.EntityScan;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+    import org.springframework.boot.persistence.autoconfigure.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -14,12 +12,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
         excludeFilters = @ComponentScan.Filter(type = FilterType.REGEX, pattern = "com\\.digibank\\..*Application"))
 @EnableJpaRepositories(basePackages = "com.digibank")
 @EntityScan(basePackages = "com.digibank")
-public class TransactionApplication extends SpringBootServletInitializer {
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(TransactionApplication.class);
-    }
+public class TransactionApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TransactionApplication.class, args);
