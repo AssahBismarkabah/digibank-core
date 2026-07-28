22bcc57dd34aacfca2004e9ce13a8f1402ff1f6b
tarthur <tcheutchouaarthur38@gmail.com>
2026-07-28 15:38:09 +0100

    addindg all postgress and my sqlx database data

--- digibank-parent/digibank-app/src/main/java/com/digibank/app/DigiBankApplication.java
+++ digibank-parent/digibank-app/src/main/java/com/digibank/app/DigiBankApplication.java
@@ -3,7 +3,7 @@ package com.digibank.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.persistence.autoconfigure.EntityScan;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
